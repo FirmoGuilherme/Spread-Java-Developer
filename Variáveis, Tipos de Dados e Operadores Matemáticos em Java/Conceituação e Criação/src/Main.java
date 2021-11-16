85bace87733a3241acba27f08a4579bb386121b0
FirmoGuilherme <firmoguilhevieira2015@gmail.com>
2021-11-16 15:59:02 -0300

    Conceituação e Criação

---
+++ Variáveis, Tipos de Dados e Operadores Matemáticos em Java/Conceituação e Criação/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // o que esta comentado é errado
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        //int _1a;
+        int _1a; // embora possa ser usado nao é uma boa pratica
+        int $aq; // embora possa ser usado nao é uma boa pratica
+
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10; // final = constante
+        // j= 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn24$678_md = 10; // embora possa ser usado nao é uma boa pratica
+        //int asrn246%78md = 10;
+
+        asrn24678md = 100;
+
+        int quantidadeProduto = 50; // camelCase
+        final int NUMERO_TENTATIVAS = 5;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn24$678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println();
+
+
+
+    }
+}
+
