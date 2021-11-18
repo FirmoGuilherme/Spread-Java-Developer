4ea08d8122c4b904a66515e5c8c8abf6dff4f399
FirmoGuilherme <firmoguilhevieira2015@gmail.com>
2021-11-18 16:06:30 -0300

    tipos de dados

---
+++ Variáveis, Tipos de Dados e Operadores Matemáticos em Java/Tipos de dados/src/main.java
@@ -0,0 +1,39 @@
+public class main {
+   public static void main(String[] args) {
+      // int i1 = -1000000000000; //
+      int i2=  28500;
+
+      // float f1 =4.5; // precisa ter o "F" no final igual no f2
+      float f2 = 10.68F;
+
+      double d1 = 85.69;
+      double d2= 99.04D;
+
+      char c1 = 'W';
+      // char c2 = 'TW'; // da erro pois char é apenas um caracter
+      char c3 = '\u0057'; // letra W em forma de codigo
+
+      String st1= "Fulano";
+      String st2= "Cicrano";
+      String st3= "Beltrano";
+
+      boolean bo1 = true;
+      boolean bo2 = false;
+
+      System.out.println(i2);
+      System.out.println(f2);
+      System.out.println(d1);
+      System.out.println(d2);
+      System.out.println(c1);
+      System.out.println(c3);
+      System.out.println(st1);
+      System.out.println(st2);
+      System.out.println(st3);
+      System.out.println(bo1);
+      System.out.println(bo2);
+
+   }
+
+
+
+}
